a9ab50a2891e454652ebacb52840473f2ff29823
hoangdinh2411 <hoangdinh2411@gmail.com>
2026-06-15 22:09:41 +0200

    add env var for domain

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,8 +1,8 @@
 export const META_DATA = {
   appName: "Myzuki",
   address: "Elinebergsplatsen 5, Helsingborg",
-  domain: "https://www.myzushi.se",
-  baseUrl: "https://www.myzushi.se",
+  domain: process.env.NEXT_PUBLIC_SITE_URL || "https://www.myzushi.se",
+  baseUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://www.myzushi.se",
   phone: "042282210",
   email: "Myzukiasian@hotmail.com",
 };
